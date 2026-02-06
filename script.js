e03f101c8356069243e9a50c7849dca751342f30
firozt <firozt03@gmail.com>
2026-02-06 12:44:46 +0000

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementsByTagName("form").addEventListener("submit", async () => {
+
+    const data = {
+        name : document.getElementById("name"),
+        email : document.getElementById("email"),
+        phone_number : document.getElementById("phone-number")
+    }
+
+})
