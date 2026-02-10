921619c1551271685521981a6d48fea98dabf297
Maryiah <marymansha15@gmail.com>
2026-02-10 14:57:07 +0000

    using sessionStorage to grab values + add dob field to form data

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,18 +1,17 @@
-document.getElementsByTagName("form")[0].addEventListener("submit", async (e) => {
+document.getElementsByTagName("form")[0].addEventListener("submit", (e) => {
     e.preventDefault();
     const form = e.target;
 
     // get data
-    const data = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        phone_number: document.getElementById("phone-number").value
+    const formData = {
+        name: form.name.value,
+        email: form.email.value,
+        phone_number: form['phonenumber'].value,
+        dob: form.dob.value
     };
-    console.log("Data to process:", data);
-    const params = new URLSearchParams(new FormData(form)).toString();
-    console.log(params)
+    sessionStorage.setItem("formData", JSON.stringify(formData));
 
-    // redirect with form data as query params
-    window.location.href = "submitted.html?" + params;
+    // Redirect to submitted page
+    window.location.href = "submitted.html";
 });
 
