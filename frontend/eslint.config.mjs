5ae1cbe7c5f36603ab75791a72ee26d5886aecf4
firozt <firozt03@gmail.com>
2026-02-17 14:29:08 +0000

    testing husky

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+
+export default defineConfig([
+  { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+    },
+  },
+]);
--- frontend/src/scripts/script.js
+++ frontend/src/scripts/script.js
@@ -1,16 +1,15 @@
-document.getElementsByTagName("form")[0].addEventListener("submit", (e) => {
-    e.preventDefault();
-    const form = e.target;
+document.getElementsByTagName('form')[0].addEventListener('submit', (e) => {
+  e.preventDefault();
+  const form = e.target;
 
-    const formData = {
-        name: form.name.value,
-        email: form.email.value,
-        phone_number: form['phonenumber'].value,
-        dob: form.dob.value
-    };
-    sessionStorage.setItem("formData", JSON.stringify(formData));
+  const formData = {
+    name: form.name.value,
+    email: form.email.value,
+    phone_number: form['phonenumber'].value,
+    dob: form.dob.value,
+  };
+  sessionStorage.setItem('formData', JSON.stringify(formData));
 
-    // Redirect to submitted page
-    window.location.href = "submitted.html";
+  // Redirect to submitted page
+  window.location.href = 'submitted.html';
 });
-
