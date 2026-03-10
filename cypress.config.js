8f73166f97a9232065437275863a1fa88e4ec04d
firozt <firozt03@gmail.com>
2026-03-10 23:35:41 +0000

    fix/using brave for CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,9 @@
 module.exports = {
   e2e: {
-    setupNodeEvents(_, config) {
-      config.browsers = [
-        {
-          name: 'Brave',
-          channel: 'stable',
-          family: 'chromium',
-          displayName: 'Brave',
-          version: '126.1.67.119',
-          path: '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',
-          majorVersion: 126,
-        },
-      ];
-      return config;
-    },
     experimentalWebKitSupport: true,
     experimentalMemoryManagement: true,
+    setupNodeEvents(_, config) {
+      return config;
+    },
   },
 };
