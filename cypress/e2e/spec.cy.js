8a6254eb0f609d76add39eb5813197ea6172dfd9
firozt <firozt03@gmail.com>
2026-03-10 19:05:38 +0000

    fix/eslint issues

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,66 @@
+const ALL_URLS = [
+  'http://mthree-peregrine-s3-3.s3-website-us-east-1.amazonaws.com/ramizmaryiah/',
+  'http://mthree-peregrine-s3-3.s3-website-us-east-1.amazonaws.com/ramizmaryiah/card.html',
+];
+
+// get all links, run all tests for all pages
+ALL_URLS.forEach((CurrentURL) => {
+  // sanity check
+  describe('sanity check', () => {
+    it('passes', () => {
+      cy.visit(CurrentURL);
+    });
+  });
+
+  // browser compatability checks
+  describe(`browser compatability tests for url ${CurrentURL}`, () => {
+    beforeEach(() => {
+      cy.visit(CurrentURL);
+    });
+
+    // checks theres an index.html (or equiv)
+    it('homepage check', () => {
+      cy.get('body').should('exist');
+    });
+
+    // check if all images loads
+    it('all images load successfully', () => {
+      cy.get('body').then(($body) => {
+        if ($body.find('img').length == 0) return; // no images
+        cy.get('img').each(($img) => {
+          cy.wrap($img)
+            .should('have.prop', 'naturalWidth')
+            .and('be.greaterThan', 0);
+        });
+      });
+    });
+
+    // check if all css files load on homepage
+    it('stylesheets load successfully', () => {
+      cy.get('link[rel="stylesheet"]').each(($link) => {
+        const href = $link.attr('href');
+        if (!href) return;
+        const url = new URL(href, CurrentURL).href;
+        cy.request(url).its('status').should('eq', 200);
+      });
+    });
+
+    // check if js files load
+    it('javscript loads successfully', () => {
+      cy.get('body').then(($body) => {
+        // get body
+        const jsScripts = $body.find('script[src$=".js"]');
+        if (jsScripts.length == 0) return; // no scripts
+        cy.get('script[src$=".js"]').each(($script) => {
+          const srcVal = $script.attr('src');
+          const url = new URL(srcVal, CurrentURL).href;
+          cy.request(url).its('status').should('eq', 200);
+          // cy.request(CurrentURL + srcVal).its('status').should('eq', 200)
+        });
+      });
+    });
+  });
+});
+
+// tests to do relating to my actual project
+describe('form page', () => {});
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,13 +5,32 @@ import globals from "globals";
 export default defineConfig([
   { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
   {
+    files: ["cypress/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.mocha,
+        cy: "readonly",
+        Cypress: "readonly",
+      },
+    },
+  },
+  {
+    files: ["cypress.config.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node, // fixes 'module is not defined'
+      },
+    },
+  },
+  {
     languageOptions: {
       globals: {
         ...globals.browser,
       },
     },
     rules: {
-      "no-unused-vars": "error",
+      "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }], // ignore params prefixed with _
       "no-undef": "error",
     },
   },
