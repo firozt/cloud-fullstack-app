98e346f7286f86cea02096f705cba2af7f930cd9
firozt <firozt03@gmail.com>
2026-03-11 00:17:07 +0000

    fix/using brave for CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,25 +1,29 @@
 const execa = require('execa');
 
+const BRAVE_PATH =
+  process.platform === 'darwin'
+    ? '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+    : '/usr/bin/brave-browser';
+
 module.exports = {
   e2e: {
     experimentalWebKitSupport: true,
     experimentalMemoryManagement: true,
     async setupNodeEvents(_, config) {
-      const result = await execa(
-        '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',
-        ['--version']
-      );
-      const [, version] = /Brave Browser ([\d.]+)/.exec(result.stdout);
+      if (BRAVE_PATH) {
+        const result = await execa(BRAVE_PATH, ['--version']);
+        const [, version] = /Brave Browser ([\d.]+)/.exec(result.stdout);
 
-      config.browsers = config.browsers.concat({
-        name: 'brave',
-        channel: 'stable',
-        family: 'chromium',
-        displayName: 'Brave',
-        version,
-        path: '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',
-        majorVersion: parseInt(version.split('.')[0]),
-      });
+        config.browsers = config.browsers.concat({
+          name: 'brave',
+          channel: 'stable',
+          family: 'chromium',
+          displayName: 'Brave',
+          version,
+          path: BRAVE_PATH,
+          majorVersion: parseInt(version.split('.')[0]),
+        });
+      }
 
       return config;
     },
