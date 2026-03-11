9065834362fea25e01485f2437bab585e85534e8
firozt <firozt03@gmail.com>
2026-03-11 00:05:13 +0000

    fix/using brave for CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,26 @@
+const execa = require('execa');
+
 module.exports = {
   e2e: {
     experimentalWebKitSupport: true,
     experimentalMemoryManagement: true,
-    setupNodeEvents(_, config) {
+    async setupNodeEvents(_, config) {
+      const result = await execa(
+        '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',
+        ['--version']
+      );
+      const [, version] = /Brave Browser ([\d.]+)/.exec(result.stdout);
+
+      config.browsers = config.browsers.concat({
+        name: 'brave',
+        channel: 'stable',
+        family: 'chromium',
+        displayName: 'Brave',
+        version,
+        path: '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',
+        majorVersion: parseInt(version.split('.')[0]),
+      });
+
       return config;
     },
   },
